394a95b736d77051fcad564e490ea3ba826d0675
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-09-23 20:26:44 +0200

    Enums

---
+++ src/types/enums.ts
@@ -0,0 +1,34 @@
+export enum PhaseType
+{
+    BREAK,
+    SESSION,
+    BOTH,
+}
+
+export enum ClockStatus
+{
+    ACTIVE,
+    PAUSED,
+}
+
+export enum ThemeType
+{
+    DARK,
+    LIGHT
+}
+
+export enum IconType
+{
+    PLUS,
+    MINUS,
+
+    PAUSE,
+    PLAY,
+    SKIP,
+    RESET,
+
+    MOON,
+    SUN,
+    INFO,
+}
+
