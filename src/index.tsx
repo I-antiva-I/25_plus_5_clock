bb24706ed56e4c5e9f6378c802f06d40108d11af
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-09-23 20:30:28 +0200

    Minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { ThemeProvider } from './providers/ThemeProvider';
+import "./assets/css/styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>
 );
